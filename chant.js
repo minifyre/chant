9081df8922ca717f97ca29c88f36d89f26f1951e
Minifyre <minifyre@gmail.com>
2018-04-26 21:54:31 -0500

    cleaned up code
    Moved functions around & documented decisions.

--- chant.js
+++ chant.js
@@ -1,47 +1,39 @@
 'use strict';
-const chant=function(initalVal={})
+const chant=function(json={})
 {
 	const
 	self={},
-	state=util.clone(initalVal);
-	self.get=function(path='')
+	state=util.clone(json),
+	receive=function(action)
 	{
 		const
-		{clone,traverse}=util,
-		props=util.path2props(path);
-		return clone(props.reduce(traverse,state));
-	};
-	self.getRef=function(props=[])
-	{
-		let ref=state;
-		for(let c=0,l=props.length;c<l;c++)
-		{
-			let prop=props[c];
-			if(!ref[prop])
-			{
-				ref[prop]={};
-			}
-			ref=ref[prop];
-		}
-		return ref;
+		{type,path,val}=action,
+		func=self[type]||(()=>'');//@note blank func protects agains non-existant func
+		func(path,val);
 	};
 	self.delete=function(path='')
 	{
-		let
-		props=util.path2props(path),
-		[firstProps,lastProps]=util.arrSplit(props,props.length-1),
-		ref=self.getRef(firstProps);
-		delete ref[lastProps];
+		let [ref,prop]=util.getRefParts(state,path);
+		delete ref[prop];
 		return self.sync({'type':'delete',path});
+	};
+	self.get=function(path='')
+	{
+		const
+		{clone,path2props,traverse}=util,
+		props=path2props(path);
+		return clone(props.reduce(traverse,state));
 	};
 	self.set=function(path='',val=undefined)
 	{
-		let
-		props=util.path2props(path),
-		[firstProps,lastProps]=util.arrSplit(props,props.length-1),
-		ref=self.getRef(firstProps);
-		ref[lastProps]=val;
+		let [ref,prop]=util.getRefParts(state,path);
+		ref[prop]=val;
 		return self.sync({type:'set',path,val});
+	};
+	self.sync=function(action)
+	{
+		//@todo send socket
+		return self;
 	};
 	self.update=function(path,func)
 	{
@@ -50,16 +42,34 @@ const chant=function(initalVal={})
 		val=func(util.clone(init));
 		return self.set(path,val);
 	};
-	self.sync=function(action)
-	{
-		//@todo send socket
-		return self;
-	};
+	//@todo onMessage(receive);
 	return self;
 },
 util={};
 util.arrSplit=(arr=[],i=arr.length)=>[arr.slice(0,i),arr.slice(i)];
 util.clone=json=>JSON.parse(JSON.stringify(json));
+util.getRef=function(ref={},props=[])
+{
+	for(let c=0,l=props.length;c<l;c++)//@note optimized for speed
+	{
+		let prop=props[c];
+		if(!ref[prop])
+		{
+			ref[prop]={};
+		}
+		ref=ref[prop];
+	}
+	return ref;
+};
+util.getRefParts=function(json={},path='')//@note the last prop will mutate, so they must be sent separately to be assembled for use
+{
+	let
+	{arrSplit,getRef,path2props}=util,
+	props=path2props(path),
+	[firstProps,lastProp]=arrSplit(props,props.length-1),
+	ref=getRef(json,firstProps);
+	return [ref,lastProp];
+};
 util.path2props=path=>path.split('.').filter(txt=>txt.length);
 util.traverse=(obj,prop)=>obj[prop];
 export {chant};

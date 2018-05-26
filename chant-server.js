b216aed6aecceb973f28bebcb735969e57987c90
Minifyre <minifyre@gmail.com>
2018-05-25 20:45:43 -0500

    changed connection auth
    Made connection authentication configurable & promise-based.

--- chant-server.js
+++ chant-server.js
@@ -8,8 +8,8 @@ async function init(httpServer,initalState={})
 	const chant=await import('./chant.mjs');
 	const
 	self=chant.chant(initalState),
-	server=new wsServer({autoAcceptConnections:false,httpServer}),
-	originIsAllowed=origin=>true;//@todo +auth logic
+	server=new wsServer({autoAcceptConnections:false,httpServer});
+	self.auth=req=>new Promise((pass,fail)=>pass());//customizable, promise-based auth
 	//needs to be reset up to use node js's crypto lib
 	self.id=function()//uuidv4 (node.js adaptation)
 	{		
@@ -18,48 +18,54 @@ async function init(httpServer,initalState={})
 	};
 	server.on('request',function(req)
 	{
-		if (!originIsAllowed(req.origin))
+		self.auth(req)
+		.then(function()
+		{
+			var connection=req.accept('echo-protocol',req.origin);
+			console.log((new Date())+' Connection accepted.');
+			connection.on('message',function(msg)
+			{
+				if (msg.type==='utf8')
+				{
+					const
+					defaults={type:'',path:'',val:''},
+					obj=JSON.parse(msg.utf8Data),
+					{type,path,val,id}=Object.assign(defaults,obj);
+					if (type==='set')
+					{
+						self[type](path,val);
+						//@todo send this action to all clients except the one it came from
+					}
+					//@todo +delete
+					else if (type==='get')
+					{
+						//@todo centeralize msg creation to always use an id
+						connection.sendUTF(JSON.stringify({type:'set',path,val:self[type](path),id:self.id()}));
+					}
+					else
+					{
+						connection.sendUTF('{"error":"'+type+' is not a valid type"}');
+					}
+					console.log('msg handled');
+				}
+				/*else if (msg.type==='binary')
+				{
+					console.log('Received Binary Message of '+msg.binaryData.length+' bytes');
+					connection.sendBytes(msg.binaryData);
+				}*/
+			});
+			connection.on('close',function(reasonCode,desc)
+			{
+				console.log((new Date())+' Peer '+connection.remoteAddress+' disconnected.');
+			});
+		})
+		.catch(function(err)
 		{
 			// Make sure we only accept requests from an allowed origin
 			req.reject();
 			console.log((new Date())+' Connection from origin '+req.origin+' rejected.');
 			return;
-		}
-		var connection=req.accept('echo-protocol',req.origin);
-		console.log((new Date())+' Connection accepted.');
-		connection.on('message',function(msg)
-		{
-			if (msg.type==='utf8')
-			{
-				const
-				defaults={type:'',path:'',val:''},
-				obj=JSON.parse(msg.utf8Data),
-				{type,path,val,id}=Object.assign(defaults,obj);
-				if (type==='set')
-				{
-					self[type](path,val);
-				}
-				else if (type==='get')
-				{
-					//@todo centeralize msg creation to always use an id
-					connection.sendUTF(JSON.stringify({type:'set',path,val:self[type](path),id:self.id()}));
-				}
-				else
-				{
-					connection.sendUTF('{"error":"'+type+' is not a valid type"}');
-				}
-				console.log('msg handled');
-			}
-			/*else if (msg.type==='binary')
-			{
-				console.log('Received Binary Message of '+msg.binaryData.length+' bytes');
-				connection.sendBytes(msg.binaryData);
-			}*/
-		});
-		connection.on('close',function(reasonCode,desc)
-		{
-			console.log((new Date())+' Peer '+connection.remoteAddress+' disconnected.');
-		});
+		})
 	});
 	return self;
 };

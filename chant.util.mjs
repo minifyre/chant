3ad0dab5a01c1debf8bbd35ee57e282895a2f529
Minifyre <minifyre@gmail.com>
2018-07-18 14:22:20 -0500

    +util mjs hardlink for node
    Added a hardlinked mjs file so that node can import it with ES6 syntax.

---
+++ chant.util.mjs
@@ -0,0 +1,23 @@
+'use strict';
+const util=
+{
+	clone:json=>JSON.parse(JSON.stringify(json)),
+	id:()=>([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,util.idHelper),
+	idHelper:c=>(c^util.rand()[0]&15>>c/4).toString(16),
+	mk:defaultObj=>(opts={})=>Object.assign(defaultObj,opts),
+	rand:()=>crypto.getRandomValues(new Uint8Array(1))
+};
+util.getRef=function(ref={},props=[])
+{
+	for(let c=0,l=props.length;c<l;c++)//@note optimized for speed
+	{
+		let prop=props[c];
+		if(!ref[prop])
+		{
+			ref[prop]={};
+		}
+		ref=ref[prop];
+	}
+	return ref;
+};
+export {util};
--- mk-mjs-hardlink.sh
+++ mk-mjs-hardlink.sh
@@ -1,1 +1,2 @@
 ln chant.js chant.mjs
+ln chant.util.js chant.util.mjs

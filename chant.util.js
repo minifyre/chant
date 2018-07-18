7d9f647173cca75ac1988a8f96ee07e17ffffc71
Minifyre <minifyre@gmail.com>
2018-07-18 13:12:03 -0500

    renamed objMaker to mk

--- chant.util.js
+++ chant.util.js
@@ -4,7 +4,7 @@ const util=
 	clone:json=>JSON.parse(JSON.stringify(json)),
 	id:()=>([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,util.idHelper),
 	idHelper:c=>(c^util.rand()[0]&15>>c/4).toString(16),
-	objMaker:defaultObj=>(opts={})=>Object.assign(defaultObj,opts),
+	mk:defaultObj=>(opts={})=>Object.assign(defaultObj,opts),
 	rand:()=>crypto.getRandomValues(new Uint8Array(1))
 };
 util.getRef=function(ref={},props=[])

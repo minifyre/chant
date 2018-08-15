bd6b597cc6df78608885b077b9db3a9e86493992
Minifyre <minifyre@gmail.com>
2018-08-15 14:27:19 -0500

    switched to async await syntax
    Changed promise syntaxes to reduce indentation & improve clarity.

--- chant.server.js
+++ chant.server.js
@@ -6,10 +6,10 @@ importHack=require('./importHack.js'),
 wsServer=require('websocket').server,
 //silo
 cache={connections:{}},
-input={},
 logic={},
 output={};
-logic.auth=req=>new Promise((pass,fail)=>pass(req));//customizable, promise-based auth
+//customizable, promise-based auth @todo add a better default
+logic.auth=req=>new Promise((pass,fail)=>pass(req));
 output.forwardAction=function(action)
 {
 	const
@@ -21,84 +21,81 @@ output.forwardAction=function(action)
 	.map(id=>cache.connections[id])
 	.forEach(connection=>connection.sendUTF(msg));
 };
-function chant(httpServer,initalState={},opts={})//@todo integrate opts
+output.promise=function(promise)
 {
-	return importHack('../node_modules/chant/')
-	.then(function(imports)
+	return promise
+	.then(data=>({data,err:null}))
+	.catch(err=>({data:null,err}));
+};
+async function chant(httpServer,initalState={},opts={})//@todo integrate opts
+{
+	const {chant,util}=await importHack('../node_modules/chant/');
+	util.rand=()=>crypto.randomBytes(1);//node crypto differs from browser crypto
+	const
+	defaults={id:util.id(),separator:'.'},
+	self=chant(initalState,Object.assign(defaults,opts)),
+	server=new wsServer({autoAcceptConnections:false,httpServer});
+	self.auth=logic.auth;
+	self.id=util.id;
+	output.disconnector=function(connectionId)
 	{
-		const {chant,util}=imports;
-		util.rand=()=>crypto.randomBytes(1);//node crypto differs from browser crypto
-		const
-		defaults={id:util.id(),separator:'.'},
-		self=chant(initalState,Object.assign(defaults,opts)),
-		server=new wsServer({autoAcceptConnections:false,httpServer});
-		self.auth=logic.auth;
-		self.id=util.id;
-		output.disconnector=function(connectionId)
+		return function(reasonCode,desc)
 		{
-			return function(reasonCode,desc)
-			{
-				delete cache.connections[connectionId];
-				//@todo delete associated tabs
-				console.clear();
-				console.log(JSON.stringify(self.delete('public.devices.'+connectionId).get(),null,4));
-			};
+			delete cache.connections[connectionId];
+			console.clear();
+			console.log(JSON.stringify(self.delete('public.devices.'+connectionId).get(),null,4));
 		};
-		server.on('request',function(req)
+	};
+	server.on('request',async function(req)
+	{
+		const {err}=await output.promise(self.auth(req));
+		if (err)//@todo Make sure to only accept requests from an allowed origin
 		{
-			self.auth(req)
-			.then(function()
+			req.reject();
+			return;
+		}
+		var connection=req.accept('echo-protocol',req.origin);
+		connection.on('message',function(msg)
+		{
+			if (msg.type==='utf8')
 			{
-				var connection=req.accept('echo-protocol',req.origin);
-				connection.on('message',function(msg)
+				const
+				defaults={type:'',path:'',val:''},
+				obj=JSON.parse(msg.utf8Data),
+				{type,path,val,device}=Object.assign(defaults,obj);
+				if (type==='set')
 				{
-					if (msg.type==='utf8')
+					self.set(path,val);
+					output.forwardAction(obj);//@todo (+evt listener & and a from:clientid prop to msg?)
+					//@todo +self.on({func:output.forwardAction})
+				}
+				else if (type==='delete')
+				{
+					self.delete(path);
+					output.forwardAction(obj);//@todo (+evt listener & and a from:clientid prop to msg?)
+				}
+				else if (type==='get')
+				{
+					cache.connections[device]=connection;
+					connection.on('close',output.disconnector(device));
+					connection.sendUTF(JSON.stringify(
 					{
-						const
-						defaults={type:'',path:'',val:''},
-						obj=JSON.parse(msg.utf8Data),
-						{type,path,val,device}=Object.assign(defaults,obj);
-						if (type==='set')
-						{
-							self.set(path,val);
-							output.forwardAction(obj);//@todo (+evt listener & and a from:clientid prop to msg?)
-							//@todo +self.on({func:output.forwardAction})
-						}
-						else if (type==='delete')
-						{
-							self.delete(path);
-							output.forwardAction(obj);//@todo (+evt listener & and a from:clientid prop to msg?)
-						}
-						else if (type==='get')
-						{
-							cache.connections[device]=connection;
-							connection.on('close',output.disconnector(device));
-							connection.sendUTF(JSON.stringify(
-							{
-								type:'set',
-								path,
-								val:self[type](path),
-								device:''
-							}));
-						}
-						else
-						{
-							connection.sendUTF('{"error":"'+type+' is not a valid type"}');
-						}
-					}
-					//@todo +msg.type==='binary' & msg.binaryData
-					console.clear();
-					console.log(JSON.stringify(self.get(),null,4));
-				});
-			})
-			.catch(function(err)
-			{
-				// Make sure we only accept requests from an allowed origin
-				req.reject();
-				return;
-			});
+						type:'set',
+						path,
+						val:self[type](path),
+						device:''
+					}));
+				}
+				else
+				{
+					connection.sendUTF('{"error":"'+type+' is not a valid type"}');
+				}
+			}
+			//@todo +msg.type==='binary' & msg.binaryData
+			console.clear();
+			console.log(JSON.stringify(self.get(),null,4));
 		});
-		return self;
 	});
+	return self;
 }
 module.exports=chant;
